6d8c3511da340b33b7a718820894b989df957125
inquityy1 <nebojsanikolic12350@yahoo.com>
2025-01-02 19:45:54 +0100

    Add register and login page styles and routes with background image

--- jobboard/resources/views/layouts/app.blade.php
+++ jobboard/resources/views/layouts/app.blade.php
@@ -40,40 +40,40 @@
 
                 <nav class="mx-auto site-navigation">
                     <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
-                    <li><a href="index.html" class="nav-link active">Home</a></li>
-                    <li><a href="about.html">About</a></li>
-                    
-                    <li><a href="profile.html">Profile</a></li>
-                    
-                    <li><a href="contact.html">Contact</a></li>
-                    <li class="d-lg-none"><a href="post-job.html"><span class="mr-2">+</span> Post a Job</a></li>
-                    @guest
-                            @if (Route::has('login'))
-                                <li class="d-lg-none"><a href="login.html">Log In</a></li>
-                            @endif
+                        <li style="padding-left: 400px"><a href="index.html" class="nav-link active">Home</a></li>
+                        <li><a href="about.html">About</a></li>
+                        
+                        <li><a href="profile.html">Profile</a></li>
+                        
+                        <li><a href="contact.html">Contact</a></li>
+                        <li class="d-lg-none"><a href="post-job.html"><span class="mr-2">+</span> Post a Job</a></li>
+                        @guest
+                                @if (Route::has('login'))
+                                    <li class=""><a href="{{ route('login') }}">Log In</a></li>
+                                @endif
 
-                            @if (Route::has('register'))       
-                                <li class="d-lg-none"><a href="register.html">Register In</a></li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))       
+                                    <li class=""><a href="{{ route('register') }}">Register</a></li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                    @endguest
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                        @endguest
 
                     
                     </ul>
@@ -92,7 +92,7 @@
         </header>
 
         <!-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm"> -->
-            <div class="container">
+            <!-- <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
@@ -101,14 +101,11 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -142,7 +139,7 @@
                         @endguest
                     </ul>
                 </div>
-            </div>
+            </div> -->
         <!-- </nav> -->
 
         <main class="py-4">
